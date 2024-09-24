751cc7908e9c23fdc0eee1889623c7bbbd53e8c4
Carlos <metawiser@gmail.com>
2024-09-24 21:35:25 +0200

    feat: Adds IssueInstance model

---
+++ src/shared/infrastructure/persistense/postgres/models/IssueInstance.ts
@@ -0,0 +1,41 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../sequelize.connect";
+
+export class IssueInstance extends Model {
+  public id!: string;
+  public message!: string;
+  public route!: string;
+  public statusCode!: number;
+  public timestamp!: Date;
+}
+
+IssueInstance.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    route: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    statusCode: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    timestamp: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    tableName: "issues",
+    timestamps: false,
+  }
+);
