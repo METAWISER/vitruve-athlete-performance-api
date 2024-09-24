e350f0d3d37f655353cf010505c7c1fa1c0b689c
Carlos <metawiser@gmail.com>
2024-09-24 22:17:11 +0200

    refactor: Adds extra properties to the Isuue entity

--- src/shared/domain/entities/Issues.ts
+++ src/shared/domain/entities/Issues.ts
@@ -1,21 +1,26 @@
-// src/shared/domain/entities/Issue.ts
 import { v4 as uuidv4 } from 'uuid';
 
 export class Issue {
   readonly id: string;
   readonly message: string;
+  readonly route: string;
+  readonly statusCode: number;
   readonly timestamp: Date;
 
-  constructor(message: string) {
-    this.id = uuidv4();
+  constructor(message: string, route: string, statusCode: number) {
+    this.id = uuidv4(); 
     this.message = message;
-    this.timestamp = new Date();
+    this.route = route;
+    this.statusCode = statusCode;
+    this.timestamp = new Date(); 
   }
 
   toPrimitives() {
     return {
       id: this.id,
       message: this.message,
+      route: this.route,
+      statusCode: this.statusCode,
       timestamp: this.timestamp.toISOString(),
     };
   }
