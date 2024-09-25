139ad099a1c3b9476cd0b287280af3b9fdd23269
Carlos <metawiser@gmail.com>
2024-09-25 12:12:29 +0200

    feat: Adds level property to Isuue entity

--- src/shared/domain/entities/Issues.ts
+++ src/shared/domain/entities/Issues.ts
@@ -1,14 +1,16 @@
-import { v4 as uuidv4 } from 'uuid';
+import { IssueLevel } from "../value-objects/IssueLevel";
+import { Uuid } from "../value-objects/Uuid";
+
 
 export class Issue {
-  readonly id: string;
+  readonly level: IssueLevel;
   readonly message: string;
   readonly route: string;
   readonly statusCode: number;
   readonly timestamp: Date;
 
-  constructor(message: string, route: string, statusCode: number) {
-    this.id = uuidv4(); 
+  constructor(readonly id: Uuid, level:IssueLevel, message: string, route: string, statusCode: number) {
+    this.level = level;
     this.message = message;
     this.route = route;
     this.statusCode = statusCode;
@@ -17,11 +19,29 @@ export class Issue {
 
   toPrimitives() {
     return {
-      id: this.id,
+      id: this.id.toString(),
+      level: this.level,
       message: this.message,
       route: this.route,
       statusCode: this.statusCode,
       timestamp: this.timestamp.toISOString(),
     };
+  }
+
+  static fromPrimitives(plainData: {
+    level: IssueLevel;
+    id: string;
+    message: string;
+    route: string;
+    statusCode: number;
+    timestamp: string;
+  }): Issue {
+    return new Issue(
+      new Uuid(plainData.id),
+      plainData.level,
+      plainData.message,
+      plainData.route,
+      plainData.statusCode
+    );
   }
 }
