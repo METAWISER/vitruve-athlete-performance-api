363c49e6b36a9419e0b51c7cd0aec3dfe4a06e79
Carlos <metawiser@gmail.com>
2024-09-25 12:20:37 +0200

    refactor: Updates error handling when a user exists

--- src/api/controllers/athletes/AthletePostController.ts
+++ src/api/controllers/athletes/AthletePostController.ts
@@ -1,26 +1,24 @@
-import { Request, Response } from "express";
-
+import { Request, Response, NextFunction } from "express";
 import { AthleteCreator } from "../../../Athletes/application/AthleteCreator";
-import  HttpResponse  from "../../../shared/infrastructure/response/HttpResponse";
-import  IController from "./IController";
+import HttpResponse from "../../../shared/infrastructure/response/HttpResponse";
+import IController from "./IController";
 import { AthleteCreatorDto } from "../../dtos/AthleteCreator.dto";
-
+import { ErrorHandler } from "../../../shared/infrastructure/error/ErrorHandler"; // Importamos el ErrorHandler
 
 export class AthletePostController implements IController {
-	constructor(
-		private readonly athleteCreator: AthleteCreator,
-		private readonly httpResponse: HttpResponse
-	) {}
+  constructor(
+    private readonly athleteCreator: AthleteCreator,
+    private readonly httpResponse: HttpResponse,
+    private readonly errorHandler: ErrorHandler // Inyectamos el ErrorHandler
+  ) {}
 
-  async run(req: Request, res: Response): Promise<void> {
-		try {
-			const athleteData: AthleteCreatorDto = req.body;
-
-			await this.athleteCreator.run(athleteData);
-
-			this.httpResponse.Created(res, { message: "Athlete created successfully" });
-		} catch (error) {
-			this.httpResponse.UnprocessableEntity(res, error);
-		}
-	}
+  async run(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const athleteData: AthleteCreatorDto = req.body;
+      await this.athleteCreator.run(athleteData);
+      this.httpResponse.Created(res, { message: "Athlete created successfully" });
+    } catch (error) {
+      await this.errorHandler.handleError(error, req, res, next);
+    }
+  }
 }
