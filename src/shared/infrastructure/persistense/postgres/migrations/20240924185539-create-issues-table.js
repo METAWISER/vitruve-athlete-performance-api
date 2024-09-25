90e6569b7a178f152cf1862ac5423c3f8b775a51
Carlos <metawiser@gmail.com>
2024-09-25 12:07:47 +0200

    feat: Adds level column to issues table and wrapped in a transaction

--- src/shared/infrastructure/persistense/postgres/migrations/20240924185539-create-issues-table.js
+++ src/shared/infrastructure/persistense/postgres/migrations/20240924185539-create-issues-table.js
@@ -3,34 +3,42 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('issues', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-      },
-      message: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      route: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      statusCode: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      timestamp: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW,
-      },
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.createTable('issues', {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4,
+        },
+        level: {
+          type: Sequelize.ENUM('info', 'warn', 'error'), 
+          allowNull: false,
+        },
+        message: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        route: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        statusCode: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        timestamp: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.NOW,
+        },
+      }, { transaction });
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('issues');
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.dropTable('issues', { transaction }); 
+    });
   }
 };
