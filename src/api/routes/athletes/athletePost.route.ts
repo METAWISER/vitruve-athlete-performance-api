6d7b11531d23a2fa52d4afebc35fd69a9814a8e7
Carlos <metawiser@gmail.com>
2024-09-25 12:02:58 +0200

    refator: Remove redundant validation

--- src/api/routes/athletes/athletePost.route.ts
+++ src/api/routes/athletes/athletePost.route.ts
@@ -1,5 +1,4 @@
-import { Router, Request, Response } from "express";
-import { body, checkExact } from "express-validator";
+import { Router, Request, Response, NextFunction } from "express";
 import { validateReqSchema } from "..";
 
 import { AthleteInstance } from "../../../Athletes/infrastructure/AthleteInstance";
@@ -7,25 +6,25 @@ import HttpResponse from "../../../shared/infrastructure/response/HttpResponse";
 import { AthleteCreator } from "../../../Athletes/application/AthleteCreator";
 import { AthleteRepository } from "../../../Athletes/infrastructure/AthleteRepository";
 import { AthletePostController } from "../../controllers/athletes/AthletePostController";
+import { ErrorHandler } from "../../../shared/infrastructure/error/ErrorHandler"; 
 
 export const register = (router: Router): void => {
-  // field validation here
-  const reqValues = [
-    body("name").exists().isString(),
-    body("age").exists().isNumeric(),
-    body("email").isEmail().isString(),
-    body("team").exists().isString(),
-  ];
-
   const athleteRepository = new AthleteRepository(AthleteInstance);
   const athleteCreator = new AthleteCreator(athleteRepository);
   const httpResponse = new HttpResponse();
-  const athletePostController = new AthletePostController(athleteCreator, httpResponse);
+  const errorHandler = new ErrorHandler();
+
+  const athletePostController = new AthletePostController(
+    athleteCreator,
+    httpResponse,
+    errorHandler
+  );
 
   router.post(
     "/athletes",
-    checkExact(reqValues),
-    validateReqSchema,
-    async (req: Request, res: Response) => await athletePostController.run(req, res),
+    validateReqSchema, 
+    async (req: Request, res: Response, next: NextFunction) => {
+      await athletePostController.run(req, res, next);
+    }
   );
 };
