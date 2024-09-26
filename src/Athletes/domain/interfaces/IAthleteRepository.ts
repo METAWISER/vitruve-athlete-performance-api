b71f4b0d91a7b240c69111f569ec1e02152ef9bb
Carlos <metawiser@gmail.com>
2024-09-26 22:59:06 +0200

    adjustment: Updates update method parameters

--- src/Athletes/domain/interfaces/IAthleteRepository.ts
+++ src/Athletes/domain/interfaces/IAthleteRepository.ts
@@ -1,9 +1,10 @@
+import { AthleteUpdateDto } from "../../../api/dtos/AthleteUpdate.dto";
 import { Athlete } from "../Athlete";
 import { AthleteId } from "./AthleteId";
 
 export interface IAthleteRepository {
   create(athlete: Athlete): Promise<void>;
-  update(athlete: Athlete): Promise<void>;
+  update(uid: AthleteId, athleteUpdateDto: AthleteUpdateDto): Promise<Athlete>;
   delete(uid: AthleteId): Promise<void>;
   search(uid: string): Promise<Athlete | []>;
   searchAll(): Promise<Athlete[]>;
