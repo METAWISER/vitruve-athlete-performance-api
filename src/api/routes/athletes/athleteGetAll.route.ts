82dbdeeb37e1709de019c1e5d5c3ae05a74eabc3
Carlos <metawiser@gmail.com>
2024-09-26 15:29:38 +0200

    feat: Adds health route implementation

---
+++ src/api/routes/athletes/athleteGetAll.route.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction, Router } from "express";
+import { AthleteInstance } from "../../../Athletes/infrastructure/AthleteInstance";
+import { AthleteGetController } from "../../controllers/athletes/AthleteGetController";
+import { AthleteRepository } from "../../../Athletes/infrastructure/AthleteRepository";
+import HttpResponse from "../../../shared/infrastructure/response/HttpResponse";
+import { ErrorHandler } from "../../../shared/infrastructure/error/ErrorHandler";
+import { AthleteGetterAll } from "../../../Athletes/application";
+
+export const register = (router: Router): void => {
+  const athleteRepository = new AthleteRepository(AthleteInstance);
+  const athleteFinder = new AthleteGetterAll(athleteRepository);
+  const httpResponse = new HttpResponse();
+  const errorHandler = new ErrorHandler();
+  const athleteGetController = new AthleteGetController(
+    athleteFinder,
+    httpResponse,
+    errorHandler
+  );
+
+  router.get(
+    "/athletes",
+    async (req: Request, res: Response, next: NextFunction) =>
+      await athleteGetController.run(req, res, next)
+  );
+};
