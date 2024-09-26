e6ee77754cb877e15da62671ebada900e1b8bce8
Carlos <metawiser@gmail.com>
2024-09-26 20:29:08 +0200

    test: Adds create method's test from athlete repository

---
+++ tests/unit/Athletes/infrastructure/AthleteRepository-create.test.ts
@@ -0,0 +1,56 @@
+import { AthleteRepository } from '../../../../src/Athletes/infrastructure/AthleteRepository';
+import { AthleteInstance } from '../../../../src/Athletes/infrastructure/AthleteInstance';
+import { Athlete } from '../../../../src/Athletes/domain/Athlete';
+import { DomainError } from '../../../../src/shared/domain/errors/DomainError';
+
+jest.mock('../../../../src/Athletes/infrastructure/AthleteInstance');
+
+describe('AthleteRepository - create', () => {
+  let repository: AthleteRepository;
+
+  beforeEach(() => {
+    repository = new AthleteRepository(AthleteInstance);
+    jest.clearAllMocks();
+  });
+
+  it('should create a new athlete if not already registered', async () => {
+    (AthleteInstance.findOne as jest.Mock).mockResolvedValue(null);
+    (AthleteInstance.create as jest.Mock).mockResolvedValue({});
+
+    const athlete = Athlete.fromPrimitives({
+      uid: 'some-uid',
+      name: 'John Doe',
+      age: 30,
+      email: 'john@example.com',
+      team: 'Team A',
+    });
+
+    await repository.create(athlete);
+    expect(AthleteInstance.create).toHaveBeenCalledWith(athlete.toPrimitives());
+  });
+
+  it('should throw an error if an athlete with the same email already exists', async () => {
+  
+    (AthleteInstance.findOne as jest.Mock).mockResolvedValue({
+      toJSON: () => ({
+        uid: 'some-uid',
+        name: 'John Doe',
+        age: 30,
+        email: 'john@example.com',
+        team: 'Team A',
+      }),
+    });
+
+    const athlete = Athlete.fromPrimitives({
+      uid: 'some-uid',
+      name: 'John Doe',
+      age: 30,
+      email: 'john@example.com',
+      team: 'Team A',
+    });
+
+    // Verificar que se lanza un error si el atleta ya existe
+    await expect(repository.create(athlete)).rejects.toThrow(DomainError);
+    expect(AthleteInstance.create).not.toHaveBeenCalled();
+  });
+});
