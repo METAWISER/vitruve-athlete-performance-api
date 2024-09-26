e50e7e1c6d5b2ea2b17d0300275df5d33910ba5c
Carlos <metawiser@gmail.com>
2024-09-26 20:22:23 +0200

    test: Adds searchAll test from athlete repository

---
+++ tests/unit/Athletes/infrastructure/AthleteRepository-searchAll.test.ts
@@ -0,0 +1,47 @@
+import { AthleteRepository } from '../../../../src/Athletes/infrastructure/AthleteRepository';
+import { AthleteInstance } from '../../../../src/Athletes/infrastructure/AthleteInstance';
+import { Athlete } from '../../../../src/Athletes/domain/Athlete';
+
+jest.mock('../../../../src/Athletes/infrastructure/AthleteInstance');
+
+describe('AthleteRepository - searchAll', () => {
+  let repository: AthleteRepository;
+
+  beforeEach(() => {
+    repository = new AthleteRepository(AthleteInstance);
+    jest.clearAllMocks();
+  });
+
+  it('should return an array of athletes', async () => {
+    const expectedAthlete = {
+      uid: 'fake-uid',
+      name: 'john doe',
+      age: '30',
+      email: 'john@example.com',
+      team: 'team a',
+    };
+
+    const expectedResult = {
+      uid: 'fake-uid',
+      name: 'john doe',
+      age: '30',
+      email: 'john@example.com',
+      team: 'team a',
+    };
+
+    (AthleteInstance.findAll as jest.Mock).mockResolvedValue([
+      {
+        toJSON: () => expectedAthlete,
+      },
+    ]);
+
+    const athletes = await repository.searchAll();
+    expect(athletes[0].toPrimitives()).toMatchObject(expectedResult);
+  });
+  it('should return an empty array if no athletes are found', async () => {
+    (AthleteInstance.findAll as jest.Mock).mockResolvedValue([]);
+
+    const athletes = await repository.searchAll();
+    expect(athletes).toHaveLength(0);
+  });
+});
