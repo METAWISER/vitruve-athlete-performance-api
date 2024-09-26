5fb96b8cbde94fce4df8dd57e27eb2853d4d9164
Carlos <metawiser@gmail.com>
2024-09-26 22:57:08 +0200

    feat: Adds update, findOne and handleExeption  methods to AthleteRepository

--- src/Athletes/infrastructure/AthleteRepository.ts
+++ src/Athletes/infrastructure/AthleteRepository.ts
@@ -3,15 +3,47 @@ import { Athlete } from "../domain/Athlete";
 import { AthleteInstance } from "./AthleteInstance";
 import { AthleteId } from "../domain/interfaces/AthleteId";
 import { DomainError } from "../../shared/domain/errors/DomainError";
+import { AthleteUpdateDto } from "../../api/dtos/AthleteUpdate.dto";
 
 export class AthleteRepository implements IAthleteRepository {
   save(save: any) {
     throw new Error('Method not implemented.');
   }
   constructor(readonly Model: typeof AthleteInstance) {}
-  update(athlete: Athlete): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async findOne(uid: string): Promise<AthleteInstance> {
+    const athlete = await this.Model.findByPk(uid);
+    if (!athlete) {
+      throw new DomainError(`Athlete with ID ${uid} not found`);
+    }
+    
+    return athlete;
   }
+  async update(uid: AthleteId, updateAthleteDto: AthleteUpdateDto): Promise<Athlete> {
+    const athlete = await this.findOne(uid.value);
+
+    if (updateAthleteDto.name) {
+      updateAthleteDto.name = updateAthleteDto.name.toLowerCase();
+    }
+
+    try {
+      await athlete.update(updateAthleteDto);
+      await athlete.save();
+
+      return Athlete.fromPrimitives(athlete.toJSON());
+    } catch (error) {
+      this.handleException(error);
+      throw error;
+    }
+  }
+
+  private handleException(error: any): void {
+    if (error instanceof DomainError) {
+      throw new DomainError(error.message);
+    }
+    throw new Error('Unexpected error occurred while updating Athlete');
+  }
+
   delete(uid: AthleteId): Promise<void> {
     throw new Error("Method not implemented.");
   }
