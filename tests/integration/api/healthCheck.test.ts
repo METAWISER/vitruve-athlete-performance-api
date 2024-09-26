35d8feb6221a5a47432136495debfd2d31c3b3b0
Carlos <metawiser@gmail.com>
2024-09-26 15:21:40 +0200

    test: Adds AthleteGetController's test

---
+++ tests/integration/api/healthCheck.test.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import Server from '../../../src/api/Server';
+
+describe('Health Check API', () => {
+  let appInstance: Server;
+
+  beforeAll(async () => {
+    appInstance = new Server('3001');
+    await appInstance.start();
+  });
+
+  afterAll(async () => {
+    await appInstance.close();
+  });
+
+  it('should return the health status', async () => {
+    const response = await axios.get('http://localhost:3001/api/v1/health');
+    expect(response.status).toBe(200);
+    expect(response.data).toMatchObject({
+      status: 'ok',
+    });
+  }, 10000);
+});
---
+++ tests/unit/Athletes/infrastructure/AthleteGetController.test.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import App from '../../../../src/api/App';
+
+describe('GET /api/v1/athletes', () => {
+  let appInstance: App;
+  let baseURL: string;
+
+  beforeAll(async () => {
+    appInstance = new App();
+    await appInstance.start();
+    baseURL = `http://localhost:3001/api/v1`;
+  });
+
+  afterAll(async () => {
+    await appInstance.close();
+  });
+
+  it('should return a list of athletes', async () => {
+    const response = await axios.get(`${baseURL}/athletes`);
+    expect(response.status).toBe(200);
+    expect(response.data).toMatchObject({
+      status: 200,
+      msg: "Success",
+      data: {
+        athletes: expect.any(Array),
+      },
+    });
+  }, 10000);
+});
