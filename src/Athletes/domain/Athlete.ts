4afbf24d688421e1744006aff95f262ce2892036
Carlos <metawiser@gmail.com>
2024-09-26 23:00:32 +0200

    refactor: Adds Athlete entity properties optional

--- src/Athletes/domain/Athlete.ts
+++ src/Athletes/domain/Athlete.ts
@@ -7,10 +7,10 @@ import { AthleteTeam } from "./interfaces/AthleteTeam";
 export class Athlete {
   constructor(
     readonly uid: AthleteId,
-    readonly name: AthleteName,
-    readonly age: AthleteAge,
-    readonly email: AthleteEmail,
-    readonly team: AthleteTeam
+    readonly name?: AthleteName,
+    readonly age?: AthleteAge,
+    readonly email?: AthleteEmail,
+    readonly team?: AthleteTeam
   ) {}
 
   static fromPrimitives(plainData: {
@@ -22,20 +22,20 @@ export class Athlete {
   }): Athlete {
     return new Athlete(
       new AthleteId(plainData.uid),
-      new AthleteName(plainData.name),
-      new AthleteAge(plainData.age),
-      new AthleteEmail(plainData.email),
-      new AthleteTeam(plainData.team)
+      plainData.name ? new AthleteName(plainData.name) : undefined,
+      plainData.age ? new AthleteAge(plainData.age): undefined,
+      plainData.email ? new AthleteEmail(plainData.email): undefined,
+      plainData.team ? new AthleteTeam(plainData.team) : undefined
     );
   }
 
   toPrimitives() {
     return {
-      uid: this.uid.toString(),
-      name: this.name.toString(),
-      age: this.age.toString(),
-      email: this.email.toString(),
-      team: this.team.toString(),
+      uid: this.uid.value,
+      name: this.name?.value,
+      age: this.age?.value,
+      email: this.email?.value,
+      team: this.team?.value,
     };
   }
 }
