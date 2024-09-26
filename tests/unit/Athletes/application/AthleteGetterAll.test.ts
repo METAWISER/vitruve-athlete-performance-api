63f58f73126ac79681e00c929918355f4a37473c
Carlos <metawiser@gmail.com>
2024-09-26 21:17:38 +0200

    test: Adds AthleteGetterAll service tests

---
+++ tests/unit/Athletes/application/AthleteGetterAll.test.ts
@@ -0,0 +1,44 @@
+import { AthleteGetterAll } from '../../../../src/Athletes/application/AthleteGetterAll';
+import { AthleteRepository } from '../../../../src/Athletes/infrastructure/AthleteRepository';
+import { Athlete } from '../../../../src/Athletes/domain/Athlete';
+import { AthleteEmail } from '../../../../src/Athletes/domain/interfaces/AthleteEmail';
+
+describe('AthleteGetterAll', () => {
+  let athleteGetterAll: AthleteGetterAll;
+  let athleteRepositoryMock: jest.Mocked<AthleteRepository>;
+
+  beforeEach(() => {
+    athleteRepositoryMock = {
+      searchAll: jest.fn(),
+    } as unknown as jest.Mocked<AthleteRepository>;
+
+    athleteGetterAll = new AthleteGetterAll(athleteRepositoryMock);
+  });
+
+  it('should return an array of athletes', async () => {
+    const mockAthletes: Athlete[] = [
+      new Athlete(
+        { value: 'some-uuid' },
+        { value: 'John Doe' },
+        { value: 30 },
+        { value: 'email@vitruve.com' } as AthleteEmail,
+        { value: 'Team A' }
+      ),
+    ];
+
+    athleteRepositoryMock.searchAll.mockResolvedValue(mockAthletes);
+
+    const result = await athleteGetterAll.run();
+
+    expect(athleteRepositoryMock.searchAll).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(mockAthletes);
+  });
+
+  it('should return an empty array if no athletes are found', async () => {
+    athleteRepositoryMock.searchAll.mockResolvedValue([]);
+
+    const result = await athleteGetterAll.run();
+    expect(athleteRepositoryMock.searchAll).toHaveBeenCalledTimes(1);
+    expect(result).toEqual([]);
+  });
+});
