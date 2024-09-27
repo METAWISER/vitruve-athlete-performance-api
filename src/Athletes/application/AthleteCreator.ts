68ae7c49c1c788255b48f85b0d15a44debc0a4e2
Carlos <metawiser@gmail.com>
2024-09-27 12:20:28 +0200

    feat: Hashes the password ones calling the AthleteCreator service

--- src/Athletes/application/AthleteCreator.ts
+++ src/Athletes/application/AthleteCreator.ts
@@ -1,3 +1,4 @@
+import { Bcrypt } from "../../shared/domain/value-objects/Bcrypt";
 import { AthleteRepository } from "../infrastructure/AthleteRepository";
 import { AthleteCreatorDto } from "../../api/dtos/AthleteCreator.dto";
 import { AthleteName } from "../domain/interfaces/AthleteName";
@@ -6,16 +7,20 @@ import { AthleteId } from "../domain/interfaces/AthleteId";
 import { Athlete } from "../domain/Athlete";
 import { AthleteEmail } from "../domain/interfaces/AthleteEmail";
 import { AthleteTeam } from "../domain/interfaces/AthleteTeam";
+import { AthletePassword } from "../domain/interfaces/AthletePassword";
 
 export class AthleteCreator {
     constructor(private repository: AthleteRepository) {}
     
     async run(data: AthleteCreatorDto): Promise<void> {
+        const hashedPassword = await Bcrypt.create(data.password);
+
         const athlete = new Athlete(
             new AthleteId(),
             new AthleteName(data.name),
             new AthleteAge(data.age),
             new AthleteEmail(data.email),
+            new AthletePassword(hashedPassword.toPrimitives()),
             new AthleteTeam(data.team)
         );
         
