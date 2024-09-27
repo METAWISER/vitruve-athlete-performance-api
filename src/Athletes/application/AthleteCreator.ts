5e9a726d77d7b648dfcaf13896bdda373f5a193f
Carlos <metawiser@gmail.com>
2024-09-27 18:21:10 +0200

    feat-fix: hashes properly the password before creating the athlete in AthleteCreator service

--- src/Athletes/application/AthleteCreator.ts
+++ src/Athletes/application/AthleteCreator.ts
@@ -10,20 +10,20 @@ import { AthleteTeam } from "../domain/interfaces/AthleteTeam";
 import { AthletePassword } from "../domain/interfaces/AthletePassword";
 
 export class AthleteCreator {
-    constructor(private repository: AthleteRepository) {}
-    
-    async run(data: AthleteCreatorDto): Promise<void> {
-        const hashedPassword = await Bcrypt.create(data.password);
+  constructor(private repository: AthleteRepository) {}
 
-        const athlete = new Athlete(
-            new AthleteId(),
-            new AthleteName(data.name),
-            new AthleteAge(data.age),
-            new AthleteEmail(data.email),
-            new AthletePassword(hashedPassword.toPrimitives()),
-            new AthleteTeam(data.team)
-        );
-        
-        await this.repository.create(athlete);
-    }
+  async run(data: AthleteCreatorDto): Promise<void> {
+    const hashedPassword = await Bcrypt.create(data.password);
+
+    const athlete = new Athlete(
+      new AthleteId(),
+      new AthleteName(data.name),
+      new AthleteAge(data.age),
+      new AthleteEmail(data.email),
+      new AthletePassword(hashedPassword.toPrimitives()),
+      new AthleteTeam(data.team)
+    );
+
+    await this.repository.create(athlete);
+  }
 }
