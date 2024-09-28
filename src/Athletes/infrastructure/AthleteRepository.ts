427b9c1540b21b9666e1587ae437993aa6b63179
Carlos <metawiser@gmail.com>
2024-09-28 19:28:59 +0200

    feat: Add new AthleteRepository implemented using prisma

--- src/Athletes/infrastructure/AthleteRepository.ts
+++ src/Athletes/infrastructure/AthleteRepository.ts
@@ -1,32 +1,40 @@
 import { IAthleteRepository } from "../domain/interfaces/IAthleteRepository";
 import { Athlete } from "../domain/Athlete";
-import { AthleteInstance } from "./AthleteInstance";
 import { AthleteId } from "../domain/interfaces/AthleteId";
 import { DomainError } from "../../shared/domain/errors/DomainError";
 import { AthleteUpdateDto } from "../../api/dtos/AthleteUpdate.dto";
+import { PrismaClient } from '@prisma/client';
 
 export class AthleteRepository implements IAthleteRepository {
-  save(save: any) {
-    throw new Error('Method not implemented.');
-  }
-  constructor(readonly Model: typeof AthleteInstance) {}
-
-  async findById(uid: string): Promise<AthleteInstance> {
-    const athlete = await this.Model.findByPk(uid);
-    if (!athlete) {
-      throw new DomainError(`Athlete with ID ${uid} not found`);
+  
+  constructor(private prisma: PrismaClient) {}
+  
+  async create(athlete: Athlete): Promise<void> {
+    const athletePrimitives = athlete.toPrimitives();
+  
+    const athleteExists = await this.prisma.athlete.findUnique({
+      where: { email: athletePrimitives.email },
+    });
+  
+    if (athleteExists) {
+      throw new DomainError("Athlete already registered with this email");
     }
     
-    return athlete;
+    await this.prisma.athlete.create({
+      data: {
+        uid: athletePrimitives.uid,
+        name: athletePrimitives.name ?? "",
+        age: athletePrimitives.age ?? 0,
+        email: athletePrimitives.email!,
+        password: athletePrimitives.password!,
+        team: athletePrimitives.team ?? "",
+      },
+    });
   }
-
-  async findByEmail(email: string): Promise<Athlete> {
-    const athlete = await this.Model.findOne({ where: { email } });
-    if (!athlete) {
-      throw new DomainError(`Athlete with email ${email} not found`);
-    }
-    
-    return Athlete.fromPrimitives(athlete.toJSON());
+  
+  async searchAll(): Promise<Athlete[]> {
+    const athletes = await this.prisma.athlete.findMany();
+    return athletes.map((athlete) => Athlete.fromPrimitives(athlete));
   }
 
   async update(uid: AthleteId, updateAthleteDto: AthleteUpdateDto): Promise<Athlete> {
@@ -37,42 +45,62 @@ export class AthleteRepository implements IAthleteRepository {
     }
 
     try {
-      await athlete.update(updateAthleteDto);
-      await athlete.save();
+      const updatedAthlete = await this.prisma.athlete.update({
+        where: { uid: uid.value },
+        data: updateAthleteDto,
+      });
 
-      return Athlete.fromPrimitives(athlete.toJSON());
+      return Athlete.fromPrimitives(updatedAthlete);
     } catch (error) {
       this.handleException(error);
       throw error;
     }
   }
 
+  
+  async findById(uid: string): Promise<Athlete> {
+    const athlete = await this.prisma.athlete.findUnique({
+      where: { uid: uid },
+    });
+    
+    if (!athlete) {
+      throw new DomainError(`Athlete with ID ${uid} not found`);
+    }
+    
+    return Athlete.fromPrimitives(athlete);
+  }
+  
+  async findByEmail(email: string): Promise<Athlete> {
+    const athlete = await this.prisma.athlete.findUnique({
+      where: { email: email },
+    });
+    
+    if (!athlete) {
+      throw new DomainError(`Athlete with email ${email} not found`);
+    }
+    
+    return Athlete.fromPrimitives(athlete);
+  }
+  
+  
   private handleException(error: any): void {
     if (error instanceof DomainError) {
       throw new DomainError(error.message);
     }
     throw new Error('Unexpected error occurred while updating Athlete');
   }
-
-  delete(uid: AthleteId): Promise<void> {
-    throw new Error("Method not implemented.");
+  
+  async delete(uid: AthleteId): Promise<void> {
+    try {
+      await this.prisma.athlete.delete({
+        where: { uid: uid.value },
+      });
+    } catch (error) {
+      this.handleException(error);
+    }
   }
+  
   search(uid: string): Promise<Athlete | []> {
     throw new Error("Method not implemented.");
-  }
-  async searchAll(): Promise<Athlete[]> {
-    const athletes = await this.Model.findAll();
-    return athletes.map((athlete) => Athlete.fromPrimitives(athlete.toJSON()));
-  }
-
-  async create(athlete: Athlete): Promise<void> {
-    const athletePrimitives = athlete.toPrimitives();
-    const athleteExists = await this.Model.findOne({
-      where: { email: athletePrimitives.email },
-    });
-    if (athleteExists) {
-      throw new DomainError("Athlete already registered with this email");
-    }
-    await this.Model.create(athletePrimitives);
   }
 }
