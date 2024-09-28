d67a63c6f50f87420fae03198a860f9334a26f92
Carlos <metawiser@gmail.com>
2024-09-28 22:14:45 +0200

    feat: Adds HealthCheckController

--- src/api/controllers/HealthCheckController.ts
+++ src/api/controllers/HealthCheckController.ts
@@ -1,12 +1,10 @@
-import { Request, Response } from "express";
-import IController from "./IController";
+import { Context } from 'hono';
 
-export class HealthCheckController implements IController{
-  public run(req: Request, res: Response): void {
-    res.status(200).json({
-      status: "ok",
-      uptime: process.uptime(),
-      timestamp: new Date(),
+export class HealthCheckController {
+  async run(c: Context): Promise<Response> {
+    return c.json({
+      status: 'healthy',
+      message: 'The server is running smoothly',
     });
   }
 }
