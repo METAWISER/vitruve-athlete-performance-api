2aa6395808c4a6024d4d1093b998fed98f8a3d89
Carlos <metawiser@gmail.com>
2024-09-29 20:57:54 +0200

    feat: Adds Metrics domain entity

---
+++ src/Metrics/domain/Metrics.ts
@@ -0,0 +1,44 @@
+import { MetricsId } from "./interfaces/MetricsId";
+import { AthleteId } from "../../Athletes/domain/interfaces/AthleteId";
+import { MetricType } from "./interfaces/MetricType";
+import { MetricUnit } from "./interfaces/MetricUnit";
+
+export class Metrics {
+  constructor(
+    readonly id: MetricsId,
+    readonly athleteId: AthleteId,
+    readonly metricType: MetricType,
+    readonly value: Number,
+    readonly unit: MetricUnit,
+    readonly timestamp: Date
+  ) {}
+
+  static fromPrimitives(plainData: {
+    id: string;
+    athleteId: string;
+    metricType: string;
+    value: Number;
+    unit: string;
+    timestamp: Date;
+  }): Metrics {
+    return new Metrics(
+      new MetricsId(plainData.id),
+      new AthleteId(plainData.athleteId),
+      new MetricType(plainData.metricType),
+      plainData.value,
+      new MetricUnit(plainData.unit),
+      new Date(plainData.timestamp)
+    );
+  }
+
+  toPrimitives() {
+    return {
+      id: this.id.value,
+      athleteId: this.athleteId.value,
+      metricType: this.metricType.value,
+      value: this.value,
+      unit: this.unit.value,
+      timestamp: this.timestamp,
+    };
+  }
+}
