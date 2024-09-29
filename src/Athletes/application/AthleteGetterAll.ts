826245a5b2685fde983653f0945cb864f5448214
Carlos <metawiser@gmail.com>
2024-09-29 17:00:26 +0200

    feat: Adds AthleteGetteraAll service

--- src/Athletes/application/AthleteGetterAll.ts
+++ src/Athletes/application/AthleteGetterAll.ts
@@ -1,11 +1,16 @@
 import { Athlete } from "../domain/Athlete";
 import { AthleteRepository } from "../infrastructure/AthleteRepository";
 
+type AthleteWithoutPassword = Omit<ReturnType<Athlete['toPrimitives']>, 'password'>;
 export class AthleteGetterAll {
   constructor(private readonly athleteRepository: AthleteRepository) {}
 
-  async run(): Promise<Athlete[]> {
+  async run(): Promise<AthleteWithoutPassword[]> {
     const athletes = await this.athleteRepository.searchAll();
-    return athletes;
+
+    return athletes.map((athlete) => {
+      const { password, ...athleteWithoutPassword } = athlete.toPrimitives();
+      return athleteWithoutPassword;
+    });
   }
 }
