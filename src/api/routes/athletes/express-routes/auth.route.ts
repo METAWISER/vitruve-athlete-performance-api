df14c76b7240c71e3e8e650c7f94082293438d57
Carlos <metawiser@gmail.com>
2024-09-29 16:52:34 +0200

    refactor: Moves express routes to their own directory

---
+++ src/api/routes/athletes/express-routes/auth.route.ts
@@ -0,0 +1,26 @@
+//TODO: Implement AthleteGetterByEmail Service
+/* import { Router, Request, Response } from "express";
+import { AuthController } from "../../controllers/athletes/AuthController";
+import HttpResponse from "../../../shared/infrastructure/response/HttpResponse";
+import { AuthService } from "../../../Athletes/application/AuthService";
+import { AthleteGetterByEmail } from "../../../Athletes/application/AthleteGetterByEmail";
+import { AthleteRepository } from "../../../Athletes/infrastructure/AthleteRepositorySequelize";
+import { AthleteInstance } from "../../../Athletes/infrastructure/AthleteInstance";
+
+export const register = (router: Router): void => {
+  const jwtSecret = process.env.JWT_SECRET ?? "";
+  const authService = new AuthService(jwtSecret);
+  const athleteRepository = new AthleteRepository(AthleteInstance);
+  const athleteGetterByEmail = new AthleteGetterByEmail(athleteRepository);
+  const httpResponse = new HttpResponse();
+  const authController = new AuthController(
+    authService,
+    athleteGetterByEmail,
+    httpResponse
+  );
+
+  router.post("/login", async (req: Request, res: Response) => {
+    await authController.login(req, res);
+  });
+};
+ */
