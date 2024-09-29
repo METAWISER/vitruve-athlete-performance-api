01fcda136d6f58654cd56a343297b25170bc9663
Carlos <metawiser@gmail.com>
2024-09-29 17:06:10 +0200

    feat: Adds route athletes to get all athletes

--- src/api/routes/athletes/athleteGetAll.route.ts
+++ src/api/routes/athletes/athleteGetAll.route.ts
@@ -1,14 +1,14 @@
-import { Request, Response, NextFunction, Router } from "express";
+import { Context, Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
 //import { AthleteInstance } from "../../../Athletes/infrastructure/AthleteInstance";
-import { AthleteGetController } from "../../controllers/athletes/AthleteGetController";
+import { AthleteGetController } from "../../controllers/athletes/AthleteGetAllController";
 import { AthleteRepository } from "../../../Athletes/infrastructure/AthleteRepository";
 import HttpResponse from "../../../shared/infrastructure/response/HttpResponse";
 import { ErrorHandler } from "../../../shared/infrastructure/error/ErrorHandler";
 import { AthleteGetterAll } from "../../../Athletes/application";
 import { validateJWT } from "../../middlewares/JwtMiddleware";
 
-export const register = (router: Router): void => {
+export const register = (router: Hono): void => {
   //Uncomment this line to use the sequelize AthleteRepository
   //const athleteRepository = new AthleteRepository(AthleteInstance);
   const athleteRepository = new AthleteRepository(new PrismaClient());
@@ -18,13 +18,12 @@ export const register = (router: Router): void => {
   const athleteGetController = new AthleteGetController(
     athleteFinder,
     httpResponse,
-    errorHandler
   );
 
   router.get(
     "/athletes",
     validateJWT,
-    async (req: Request, res: Response, next: NextFunction) =>
-      await athleteGetController.run(req, res, next)
+    async (c: Context) =>
+      await athleteGetController.run(c)
   );
 };
