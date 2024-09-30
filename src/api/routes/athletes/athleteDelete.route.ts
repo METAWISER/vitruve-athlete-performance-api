b739594ac4d4e5a8159df7fcc5b8a708956ae0d0
Carlos <metawiser@gmail.com>
2024-09-30 23:59:26 +0200

    feat: Adds delete route /athlete/:id to deletes athletes and its performance metrics

---
+++ src/api/routes/athletes/athleteDelete.route.ts
@@ -0,0 +1,21 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+import { AthleteRepository } from "../../../Athletes/infrastructure/AthleteRepository";
+import { MetricsRepository } from "../../../Metrics/infrastructure/MetricsRepository";
+import { AthleteDeleteService } from "../../../Athletes/application/AthleteDeleter";
+import { AthleteDeleteController } from "../../controllers/athletes/AthleteDeleteController";
+import HttpResponse from "../../../shared/infrastructure/response/HttpResponse";
+
+export const register = (router: Hono): void => {
+  const prisma = new PrismaClient();
+  
+  const athleteRepository = new AthleteRepository(prisma);
+  const metricsRepository = new MetricsRepository(prisma);
+  
+  const athleteDeleteService = new AthleteDeleteService(athleteRepository, metricsRepository);
+  const httpResponse = new HttpResponse();
+
+  const athleteDeleteController = new AthleteDeleteController(athleteDeleteService, httpResponse);
+
+  router.delete("/athletes/:id", async (c) => await athleteDeleteController.run(c));
+};
