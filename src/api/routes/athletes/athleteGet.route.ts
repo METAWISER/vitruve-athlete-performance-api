d83713b8e1736f53506e99f6ea26dd672c989be0
Carlos <metawiser@gmail.com>
2024-09-30 23:05:50 +0200

    feat: Adds /athletes/:id route to get athlete and its performance metrics

---
+++ src/api/routes/athletes/athleteGet.route.ts
@@ -0,0 +1,26 @@
+import { Hono } from "hono";
+import { AthleteGetController } from "../../controllers/athletes/AthleteGetController";
+import { PrismaClient } from "@prisma/client";
+import { AthleteRepository } from "../../../Athletes/infrastructure/AthleteRepository";
+import { MetricsRepository } from "../../../Metrics/infrastructure/MetricsRepository";
+import { AthleteMetricsGetter } from "../../../Metrics/application/MetricsGetter";
+import { AthleteGetService } from "../../../Athletes/application/AthleteGetter";
+import HttpResponse from "../../../shared/infrastructure/response/HttpResponse";
+
+export const register = (router: Hono): void => {
+  const prisma = new PrismaClient();
+  const athleteRepository = new AthleteRepository(prisma);
+  const metricsRepository = new MetricsRepository(prisma);
+  const athleteMetricsGetter = new AthleteMetricsGetter(metricsRepository);
+  const athleteGetService = new AthleteGetService(athleteRepository, athleteMetricsGetter);
+  const httpResponse = new HttpResponse();
+
+  const athleteGetController = new AthleteGetController(
+    athleteGetService,
+    httpResponse,
+  );
+
+  router.get("/athletes/:id", async (c) => {
+    return await athleteGetController.run(c);
+  });
+};
