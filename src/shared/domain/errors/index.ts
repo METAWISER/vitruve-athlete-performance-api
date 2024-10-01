31c6d85182031a379bffc61d75664ea7c40d68ba
Carlos <metawiser@gmail.com>
2024-10-01 12:37:07 +0200

    feat: creates index.ts fon error clases

---
+++ src/shared/domain/errors/index.ts
@@ -0,0 +1,5 @@
+export {ConflictError} from './ConfictError';
+export {DomainError} from './DomainError';
+export {DatabaseError} from './DatabaseError';
+export {InternalServerError} from './InternalServerError';
+export {ValidationError} from './ValidationError';
