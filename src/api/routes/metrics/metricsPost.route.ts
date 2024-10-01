9c2b546191acb26932493fe7b3175c2bce6d0659
Carlos <metawiser@gmail.com>
2024-10-01 12:39:52 +0200

    fix: Updates metricPost route

--- src/api/routes/metrics/metricsPost.route.ts
+++ src/api/routes/metrics/metricsPost.route.ts
@@ -2,13 +2,18 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
 import { MetricsPostController } from "../../controllers/metrics/MetricsCreatorController";
 import { MetricsRepository } from "../../../Metrics/infrastructure/MetricsRepository";
+import { AthleteRepository } from "../../../Athletes/infrastructure/AthleteRepository";
 import { MetricsCreator } from "../../../Metrics/application/MetricsCreator";
 import HttpResponse from "../../../shared/infrastructure/response/HttpResponse";
 
 export const register = (router: Hono): void => {
   const prisma = new PrismaClient();
   const metricsRepository = new MetricsRepository(prisma);
-  const metricsCreator = new MetricsCreator(metricsRepository);
+  const athleteRepository = new AthleteRepository(prisma);
+  const metricsCreator = new MetricsCreator(
+    metricsRepository,
+    athleteRepository
+  );
   const httpResponse = new HttpResponse();
 
   const metricsPostController = new MetricsPostController(
